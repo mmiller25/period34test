a64971078d5414f8acd314fb2e7a63479485456b
Quincy Holmer <qholmer@schools.nyc.gov>
2025-01-10 10:08:56 -0500

    changed stuff

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -2,5 +2,10 @@ public class AnotherClass {
     private int x;
     public AnotherClass() {
         x = 10;
+        System.out.println("intitialized");
+    }
+
+    public int getX() {
+        return x;
     }
 }
